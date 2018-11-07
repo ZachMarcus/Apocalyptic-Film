6fdf2c92bbd9624aec6717a27498f91a138c4d37
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-11-07 15:33:46 -0500

    Reclassified some films

--- data/subgenres.js
+++ data/subgenres.js
@@ -1,5 +1,5 @@
 
 // list of subgenres within apocalyptic films
 var groups = new vis.DataSet([ // 1-indexed
-{id: 1, visible: true, content: "Natural Disaster"},{id: 2, visible: true, content: "Aliens"},{id: 3, visible: true, content: "Post-Atomic War"},{id: 4, visible: true, content: "Atomic War"},{id: 5, visible: true, content: "Technological Advancement"},{id: 6, visible: true, content: "Biological Plague"},{id: 7, visible: true, content: "Vampiric Plague"},{id: 8, visible: true, content: "Zombie Plague"},{id: 9, visible: true, content: "Rapture"},{id: 10, visible: true, content: "Environmental Destruction"},{id: 11, visible: true, content: "Revelation"},{id: 12, visible: true, content: "Psychological Plague"},{id: 13, visible: true, content: "Dragons"},
+{id: 1, visible: true, content: "Natural Disaster"},{id: 2, visible: true, content: "Aliens"},{id: 3, visible: true, content: "Post-Atomic War"},{id: 4, visible: true, content: "Atomic War"},{id: 5, visible: true, content: "Technological Advancement"},{id: 6, visible: true, content: "Biological Plague"},{id: 7, visible: true, content: "Zombie Plague"},{id: 8, visible: true, content: "Rapture"},{id: 9, visible: true, content: "Environmental Destruction"},{id: 10, visible: true, content: "Revelation"},{id: 11, visible: true, content: "Psychological Plague"},{id: 12, visible: true, content: "Dragons"},
 ]);
